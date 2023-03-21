dc53f24ebcfd83b39969f66fa24e54f2c37ca306
ehurafa <rafarfl2010@gmail.com>
2023-03-20 21:35:08 -0400

    Property Binding

--- first-project/src/app/data-biding/data-biding.component.ts
+++ first-project/src/app/data-biding/data-biding.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class DataBidingComponent {
   public name: string = "Rafael"
   public age: number = 32
+  public checkedDisabled: boolean = true
+  public imgSrc: string = "https://p.turbosquid.com/ts-thumb/OH/0A9fga/T1/rr_0000/jpg/1671401940/600x600/fit_q87/c28eb073e59dd01e9092280c093fc056c7634a85/rr_0000.jpg"
+  public imgTitle: string = "Título da imagem"
+
 }
