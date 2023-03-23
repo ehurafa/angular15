37f4b6097031620773eee8c659fec8cc018ae346
ehurafa <rafarfl2010@gmail.com>
2023-03-22 21:11:25 -0400

    ngStyle

--- first-project/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ first-project/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -6,15 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.scss']
 })
 export class DiretivasAtributosComponent implements OnInit {
-  public value: boolean = false 
-
+  public value: boolean = false;
+  public heightPx: string = '20px';
 
   ngOnInit(): void {
     setInterval(() => {
       if (this.value) {
-        this.value = false
+        this.value = false;
       } else {
-        this.value = true
+        this.value = true;
+      }
+
+      if (this.heightPx == '20px') {
+        this.heightPx = '50px';
+      } else {
+        this.heightPx = '20px';
       }
     }, 2000)
   }
