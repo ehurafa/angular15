5712fbb0c1c72473f454e8e7d25a05f4ee73c9ae
ehurafa <rafarfl2010@gmail.com>
2023-03-30 11:20:49 -0400

    hands on 7 taskList validation

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+
+  public taskList: Array<TaskList> = [
+    { task: "Minha task", checked: true },
+    { task: "Minha task 2", checked: false }
+  ];
 
 }
