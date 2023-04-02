0e1a8a97724f27e7880fd2d791ab9967ec9cb128
ehurafa <rafarfl2010@gmail.com>
2023-04-02 16:09:06 -0400

    hands on 11 - save data in localstorage

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -3,15 +3,16 @@ import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
+
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   ngDoCheck() {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();
   }
 
   public deleteItemTaskList(event: number) {
@@ -35,6 +36,13 @@ export class TodoListComponent implements DoCheck {
       if(confirm) {
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage() {
+    if(this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 
