10a8d93a2b84d63decaad6489722ddf9fb752091
ehurafa <rafarfl2010@gmail.com>
2023-04-12 14:38:13 -0400

    formGroup

---
+++ reactve-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.sass']
+})
+export class ReactiveFormsComponent {
+
+  public registerForm: FormGroup = this.fb.group({
+    firstName: [''],
+    lastName: ['']
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  public submitForm() {
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.value.firstName);
+    console.log(this.registerForm.value.lastName);
+  }
+
+}
