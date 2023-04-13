68b4a02abeb2a4811899148b3e7c282b9bc72294
ehurafa <rafarfl2010@gmail.com>
2023-04-13 16:39:50 -0400

    create routes

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
-  // { path: "", component: PageHomeComponent, pathMatch: 'full' }
+  { path: "", component: HomeComponent, pathMatch: 'full' },
+  { path: "about", component: AboutComponent },
+  { path: "404", component: ErrorComponent },
 ];
 
 @NgModule({
