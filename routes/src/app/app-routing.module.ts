b1b24a61932d69135638f327de2c861d27f9afab
ehurafa <rafarfl2010@gmail.com>
2023-04-13 21:54:37 -0400

    route parameters

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: 'full' },
-  { path: "about", component: AboutComponent },
+  { path: "about/:id/:username", component: AboutComponent },
   { path: "404", component: ErrorComponent },
   { path: "**", redirectTo: '404' }
 ];
--- routes/src/app/pages/about/about.component.ts
+++ routes/src/app/pages/about/about.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  constructor(private activeRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe(
+      res => console.log(res['id'], res['username'], res)
+    )
+
+    this.activeRoute.queryParams.subscribe(
+      res => console.log('params ', res)
+    )
+  }
 }
