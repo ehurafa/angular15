bd2f9c134d5319d1cf6d82a239f5365aa9aea74e
ehurafa <rafarfl2010@gmail.com>
2023-04-13 20:07:10 -0400

    redirectTo

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: 'full' },
   { path: "about", component: AboutComponent },
   { path: "404", component: ErrorComponent },
+  { path: "**", redirectTo: '404' }
 ];
 
 @NgModule({
