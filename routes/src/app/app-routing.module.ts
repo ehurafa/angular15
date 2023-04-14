7d1b165075f74100222db92735cb923d2a569154
ehurafa <rafarfl2010@gmail.com>
2023-04-14 18:40:43 -0400

    children routes

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: 'full' },
+  { path: "about", component: AboutComponent, children: [
+    { path: 'foo', component: AboutComponent }
+  ]},
   { path: "about/:id/:username", component: AboutComponent },
   { path: "404", component: ErrorComponent },
   { path: "**", redirectTo: '404' }
