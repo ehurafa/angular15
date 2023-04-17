53c4577b479f10f9bfbe9f2dfa35ebdff0453bd2
ehurafa <rafarfl2010@gmail.com>
2023-04-17 13:26:59 -0400

    unit test from component

--- unit-tests/src/app/shared/banking/banking.component.spec.ts
+++ unit-tests/src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,30 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('(U) getSavings: shoud savings have = 10', () => {
+    expect(component.getSavings).toEqual(10);
+  });
+
+  it('(U) getWallet: shoud savings have = 50', () => {
+    expect(component.getWallet).toEqual(50);
+  });
+
+  it('(U) setDraw: shoud transfer savings from wallet', () => {
+    component.setDraw('10');
+    // fixture.detectChanges(); 
+
+    expect(component.getSavings).toEqual(0);
+    expect(component.getWallet).toEqual(60);
+  });
+
+  it('(U) setDeposit: shoud transfer wallet from savings', () => {
+    component.setDeposit('50');
+    // fixture.detectChanges(); 
+
+    expect(component.getWallet).toEqual(0);
+    expect(component.getSavings).toEqual(60);
+  });
+
+
 });
